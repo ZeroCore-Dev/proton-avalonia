357c818212b182e4d2ac081f94b64f193a9c9ed1
rouVling <youqianiqian@126.com>
2024-02-23 17:32:05 +0800

    修改项目名称

---
+++ Views/QuickWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace proton_avalonia.Views
+{
+    public partial class QuickWindow : Window
+    {
+        public QuickWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
