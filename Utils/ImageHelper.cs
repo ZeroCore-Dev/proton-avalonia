7d79b8a700cb328ec730ef706d391b8f4875f2fc
rouVling <youqianiqian@126.com>
2024-02-24 20:34:30 +0800

    TODO: debug

---
+++ Utils/ImageHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace proton_avalonia.Utils
+{
+    public static class ImageHelper
+    {
+        public static Bitmap LoadFromResource(Uri resourceUri)
+        {
+            var tmp =  new Bitmap(AssetLoader.Open(resourceUri));
+            Debug.Assert(tmp != null);
+            return tmp;
+        }
+
+        public static async Task<Bitmap?> LoadFromWeb(Uri url)
+        {
+            using var httpClient = new HttpClient();
+            try
+            {
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var data = await response.Content.ReadAsByteArrayAsync();
+                return new Bitmap(new MemoryStream(data));
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"An error occurred while downloading image '{url}' : {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
--- ViewModels/QuickWindowViewModel.cs
+++ ViewModels/QuickWindowViewModel.cs
@@ -1,14 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using System.IO;
+using System.Net.Http;
+using Avalonia;
+using Avalonia.Platform;
+
+using proton_avalonia.Utils;
+using System.ComponentModel;
+
 
 namespace proton_avalonia.ViewModels
 {
     public class QuickWindowViewModel: ViewModelBase
     {
 
+        public Bitmap? QuickIconFromResource { get; set; } = ImageHelper.LoadFromResource(new Uri("avares://proton_avalonia/Assets/SearchIcon.png"));
+        public Task<Bitmap?>? QuickIconFromWebsite { get; } = ImageHelper.LoadFromWeb(new Uri("https://upload.wikimedia.org/wikipedia/commons/4/41/NewtonsPrincipia.jpg"));
+
+
+        public string WatermarkPrompt { get; set; } = "Type here to search...";
+
+        //public event PropertyChangedEventHandler? PropertyChanged;
+
+        //protected virtual void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
 
     }
+
+    //public partial class QuickWindowViewModel: ObservableObject
+    //{
+
+    //    [ObservableProperty]
+    //    private Bitmap? _QuickIconFromResource = ImageHelper.LoadFromResource(new Uri("avares://proton_avalonia/Assets/SearchIcon.png"));
+
+    //    [ObservableProperty]
+    //    private Task<Bitmap?>? _QuickIconFromWebsite;
+
+    //    [ObservableProperty]
+    //    private string _WatermarkPrompt = string.Empty;
+
+    //}
 }
